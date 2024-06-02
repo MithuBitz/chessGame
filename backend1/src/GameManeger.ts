7016223cb3fdedd9ed3bff849a2e72f717e34af1
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-06-03 01:08:19 +0530

    Create the GameManeger and the logic

---
+++ backend1/src/GameManeger.ts
@@ -0,0 +1,41 @@
+import { WebSocket } from "ws";
+import { INIT_GAME } from "./message";
+
+//Create a Game manager class to create a game when two players are connected
+export class GameManager {
+  private games: Game[];
+  private pendingUser: WebSocket;
+  private users: WebSocket[];
+
+  constructor() {
+    this.games = [];
+  }
+
+  //add a user or a player
+  addUser(socket: WebSocket) {
+    //add the user from socket
+    this.users.push(socket);
+    this.addHandler(socket);
+  }
+
+  removeUser(socket: WebSocket) {
+    this.users = this.users.filter((user) => user !== socket);
+    //Stop the game here bcoz the player is removed
+  }
+
+  private addHandler(socket: WebSocket) {
+    socket.on("message", (data) => {
+      const message = JSON.parse(data.toString());
+
+      if (message.type === INIT_GAME) {
+        //If the user or player is waiting for the player then
+        if (this.pendingUser) {
+          //Start the gaem
+        } else {
+          //if the user not waiting for next player then
+          this.pendingUser = socket;
+        }
+      }
+    });
+  }
+}
--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,13 +1,13 @@
 import { WebSocketServer } from "ws";
+import { GameManager } from "./GameManeger";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const gameManeger = new GameManager();
+
 wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
+  //add the socket as a user
+  gameManeger.addUser(ws);
 
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
-  });
-
-  ws.send("something");
+  ws.on("disconnect", () => gameManeger.removeUser(ws));
 });
